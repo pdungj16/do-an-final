1ec0c51ec0217e4dc641d53d2fa3f66166329cec
Giang Nguyen Vu Truong <pdungj16@gmail.com>
2024-05-02 16:26:36 +0700

    test 7

--- src/app/menu-items/page.js
+++ src/app/menu-items/page.js
@@ -44,8 +44,7 @@ export default function MenuItemsPage() {
                 <h2 className="text-sm text-gray-500 mt-8">Edit menu item:</h2>
                 <div className="grid grid-cols-3 gap-2">
                 {menuItems?.length > 0 && menuItems.map(item => (
-                    <div key={item._id}>
-                        <Link href={'/menu-items/edit/'+item._id} className="bg-gray-200 rounded-lg p-4">
+                        <Link key={item._id} href={'/menu-items/edit/'+item._id} className="w-full bg-gray-200 rounded-lg p-4">
                             <div className="relative">
                                 <Image 
                                 className="rounded-md"
@@ -55,7 +54,6 @@ export default function MenuItemsPage() {
                                 {item.name}
                             </div>
                         </Link>
-                    </div>
                 ))}
                 </div>
             </div>
