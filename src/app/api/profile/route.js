5e4ac75486f1075bbbb60c6eeb6668d9e6c055d1
Giang Nguyen Vu Truong <pdungj16@gmail.com>
2024-05-02 18:55:34 +0700

    test 14

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import { getServerSession } from "next-auth";
-import {authOptions} from "./../auth/[...nextauth]/route";
+import {authOptions} from "./../auth/[...nextauth]";
 import { User } from "../../models/User";
 import {UserInfo} from "../../models/UserInfo";
 
