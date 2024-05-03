83d6e3f58fe79617cacc82e559507f84abc37645
unknown <pdung100bx.com>
2024-05-03 22:34:59 +0700

    test 12

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
@@ -6,52 +6,60 @@ import { useContext } from "react";
 import { CartContext } from "../AppContext";
 import ShoppingCart from "../icons/ShoppingCart";
 
-export default function Header(){
-    const session = useSession();
-    const status = session?.status;
-    const userData = session.data?.user;
-    let userName = userData?.name || userData?.email;
-    const {cartProducts} = useContext(CartContext);
-    if (userName && userName.includes(' ')) {
-      userName = userName.split(' ')[0];
-    }
-    return(
-        <header className="flex items-center justify-between ">
-        
-        <nav className="flex items-center gap-8 text-gray-500 font-semibold">
-        <Link className="text-primary font-semibold text-3xl" href={'/'}>ST PIZZA</Link>
-          <Link href={'/'}>Home</Link>
-          <Link href={'/menu'}>Menu</Link>
-          <Link href={'/#about'}>About</Link>
-          <Link href={'/#contact'}>Contact</Link>
-          
-        </nav>
-         <nav className="flex items-center gap-4 text-gray-500 font-semibold">
-          {status === 'authenticated' && (
-            <>
-            <Link href={'/profile'} className="whitespace-nowrap">
-              Hello, {userName}</Link>
-            <button 
-            onClick={() => signOut()}
-            className="bg-primary rounded-full text-white px-8 py-2">Logout</button>
-            </>
-            
+export default function Header() {
+  const session = useSession();
+  const status = session?.status;
+  const userData = session.data?.user;
+  let userName = userData?.name || userData?.email;
+  const { cartProducts } = useContext(CartContext);
+  if (userName && userName.includes(" ")) {
+    userName = userName.split(" ")[0];
+  }
+  return (
+    <header className="flex items-center justify-between ">
+      <nav className="flex items-center gap-8 text-gray-500 font-semibold">
+        <Link className="text-primary font-semibold text-3xl" href={"/"}>
+          ST PIZZA
+        </Link>
+        <Link href={"/"}>Home</Link>
+        <Link href={"/menu"}>Menu</Link>
+        <Link href={"/#about"}>About</Link>
+        <Link href={"/#contact"}>Contact</Link>
+      </nav>
+      <nav className="flex items-center gap-4 text-gray-500 font-semibold">
+        {status === "authenticated" && (
+          <>
+            <Link href={"/profile"} className="whitespace-nowrap">
+              Hello, {userName}
+            </Link>
+            <button
+              onClick={() => signOut()}
+              className="bg-primary rounded-full text-white px-8 py-2"
+            >
+              Logout
+            </button>
+          </>
+        )}
+        {status === "unauthenticated" && (
+          <>
+            <Link href={"/login"}>Login</Link>
+            <Link
+              href={"/register"}
+              className="bg-primary rounded-full text-white px-8 py-2"
+            >
+              Register
+            </Link>
+          </>
+        )}
+        <Link href={"/cart"} className="relative">
+          <ShoppingCart />
+          {cartProducts?.length > 0 && (
+            <span className="absolute -top-2 -right-4 bg-primary text-white text-xs py-1 px-1 rounded-full leading-3">
+              {cartProducts.length}
+            </span>
           )}
-          {status === 'unauthenticated' && (
-            <>
-            <Link href={'/login'}>Login</Link>
-         <Link href={'/register'} className="bg-primary rounded-full text-white px-8 py-2">Register</Link>
-            </>
-          )}      
-          <Link href={'/cart'} className="relative">
-            <ShoppingCart />
-            {cartProducts?.length > 0 && (
-              <span className="absolute -top-2 -right-4 bg-primary text-white text-xs py-1 px-1 rounded-full leading-3">
-            {cartProducts.length}
-          </span>
-            )}
-          </Link>
-         </nav>
-      </header>
-    );
+        </Link>
+      </nav>
+    </header>
+  );
 }
