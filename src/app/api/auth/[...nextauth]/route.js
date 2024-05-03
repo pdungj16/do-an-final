6562a79ffe03295edd5e3e9a57db146e8c51452c
unknown <pdung100bx.com>
2024-05-03 15:57:57 +0700

    test 3

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,13 +1,12 @@
 import clientPromise from "../../../../libs/mongoConnect";
 import mongoose from "mongoose";
 import { UserInfo } from "../../../models/UserInfo";
-import NextAuth, {getServerSession} from "next-auth";
-import { User } from "../../../models/User"
-import bcrypt from 'bcrypt';
-import CredentialsProvider from "next-auth/providers/credentials"
+import NextAuth, { getServerSession } from "next-auth";
+import { User } from "../../../models/User";
+import bcrypt from "bcrypt";
+import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
-import { MongoDBAdapter } from "@auth/mongodb-adapter"
-import testAuth from '../lib/auth';
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
 
 export const authOptions = {
   secret: process.env.SECRET,
@@ -18,10 +17,14 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     CredentialsProvider({
-      name: 'Credentials',
-      id: 'credentials',
+      name: "Credentials",
+      id: "credentials",
       credentials: {
-        username: { label: "Email", type: "email", placeholder: "test@example.com" },
+        username: {
+          label: "Email",
+          type: "email",
+          placeholder: "test@example.com",
+        },
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials, req) {
@@ -29,16 +32,16 @@ export const authOptions = {
         const password = credentials?.password;
 
         mongoose.connect(process.env.MONGO_URL);
-        const user = await User.findOne({email});
+        const user = await User.findOne({ email });
         const passwordOk = user && bcrypt.compareSync(password, user.password);
 
         if (passwordOk) {
           return user;
         }
 
-        return null
-      }
-    })
+        return null;
+      },
+    }),
   ],
 };
 
@@ -48,7 +51,7 @@ export async function isAdmin() {
   if (!userEmail) {
     return false;
   }
-  const userInfo = await UserInfo.findOne({email:userEmail});
+  const userInfo = await UserInfo.findOne({ email: userEmail });
   if (!userInfo) {
     return false;
   }
@@ -57,4 +60,4 @@ export async function isAdmin() {
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
